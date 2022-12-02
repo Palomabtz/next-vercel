e1df9895127dde2f57488380566178f265d1684d
PalomaBtz <paloma.benitez@intersel.com.mx>
2022-12-01 17:53:32 -0700

    inicio

---
+++ components/layouts/DarkLayout.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+export const DarkLayout: FC = ({ children }) => {
+  return (
+    <div style={{backgroundColor: 'darkblue', borderRadius: '5px', padding: '10px'}}>
+        <h3>Dark layout</h3>
+        <div>
+        {children}
+        </div>
+    </div>
+  );
+}
